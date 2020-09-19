848ebffa3ec2fe8eaeb1e4e3f8abf736a356469e
Jakub Mularski <jakub_mularski@hotelmap.com>
2020-09-19 23:54:23 +0200

    huawei added

---
+++ app/src/main/java/com/example/addictionapp/utils/HmsUtils.kt
@@ -0,0 +1,19 @@
+package com.example.addictionapp.utils;
+
+import android.content.Context
+import com.huawei.agconnect.config.AGConnectServicesConfig
+import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.api.ConnectionResult
+import com.huawei.hms.api.HuaweiApiAvailability
+
+object HmsUtils {
+
+        fun isHmsAvailable(context:Context):Boolean=
+        HuaweiApiAvailability.getInstance()
+        .isHuaweiMobileServicesAvailable(context)==ConnectionResult.SUCCESS
+
+        fun getPushNotificationsToken(context:Context):String {
+                val appId = AGConnectServicesConfig.fromContext(context).getString("client/app_id")
+                return HmsInstanceId.getInstance(context).getToken(appId, "HCM")
+        }
+}
