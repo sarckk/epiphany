91bc35908ee030737cf2c8648bd1edf307227322
Jakub Mularski <jakub_mularski@hotelmap.com>
2020-10-07 18:23:05 +0200

    feat(service): finalised service work

--- app/src/main/java/com/example/addictionapp/services/AppTrackingService.kt
+++ app/src/main/java/com/example/addictionapp/services/AppTrackingService.kt
@@ -9,6 +9,7 @@ import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavDeepLinkBuilder
 import com.example.addictionapp.R
 import com.example.addictionapp.data.blocklist.BlocklistRepository
 import org.koin.android.ext.android.inject
@@ -17,29 +18,49 @@ class AppTrackingService : Service() {
     private val CHANNEL_ID = "Epiphany"
     private val repo: BlocklistRepository by inject()
 
-    class AppTracker(repo: BlocklistRepository): Thread() {
-
+    class AppTracker(repo: BlocklistRepository, context: Context): Thread() {
+        val context = context
         val applicationsPackageNames = repo.getAllBlacklistedApps().map {
             it.packageName
         }
 
+        var isBlocklistedAppRunning = false
+
         override fun run() {
 
             while (true) {
-                val usageEvents = usageStatsManager.queryEvents(System.currentTimeMillis() - 5000, System.currentTimeMillis())
+                val usageEvents = usageStatsManager.queryEvents(System.currentTimeMillis() - 10000, System.currentTimeMillis())
 
+                var blocklistedAppInUsageEvents = false
                 while (usageEvents.hasNextEvent()) {
                     var event: UsageEvents.Event = UsageEvents.Event()
                     val success = usageEvents.getNextEvent(event)
                     if (success) {
-                        Log.d("Epiphany",
-                            applicationsPackageNames.contains(event.packageName).toString()
-                        )
+                        if (applicationsPackageNames.contains(event.packageName)) {
+                            blocklistedAppInUsageEvents = true
+                            isBlocklistedAppRunning = true
+                        }
+
+
                     }
                 }
 
-                sleep(5000)
+                if (isBlocklistedAppRunning && !blocklistedAppInUsageEvents) {
+                    isBlocklistedAppRunning = false
+
+                    //launch reflection intent
+                    NavDeepLinkBuilder(context)
+                        .setGraph(R.navigation.nav_graph)
+                        .setDestination(R.id.wellbeingStateFragment)
+                        .createPendingIntent()
+                }
+
+
+
+                sleep(10000)
             }
+
+
         }
     }
 
@@ -74,7 +95,7 @@ class AppTrackingService : Service() {
             .setTicker(getText(R.string.ticker_text))
             .build()
 
-        val tracker = AppTracker(repo)
+        val tracker = AppTracker(repo, this)
         tracker.start()
 
         startForeground(1, notification)
--- app/src/main/java/com/example/addictionapp/ui/apps/AppSelectionActivity.kt
+++ app/src/main/java/com/example/addictionapp/ui/apps/AppSelectionActivity.kt
@@ -3,24 +3,14 @@ package com.example.addictionapp.ui.apps
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.addictionapp.R
-import com.example.addictionapp.data.models.Application
 import com.example.addictionapp.data.models.ApplicationWithIcon
-import com.example.addictionapp.data.models.Reflection
 import com.example.addictionapp.ui.MainActivity
-import com.example.addictionapp.ui.reflection.create.WhatElseViewModel
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import kotlinx.android.synthetic.main.activity_app_selection.*
-import kotlinx.android.synthetic.main.fragment_overview.*
-import kotlinx.android.synthetic.main.item_application_selection.view.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class AppSelectionActivity : AppCompatActivity() {
